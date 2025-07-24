3f8ea38b3237ac38db621982f11bf4ed925e8b16
Derin Karas <127653224+derinkaras@users.noreply.github.com>
2025-07-23 18:42:02 -0600

    Fixed the qr url

--- utils/qr.js
+++ utils/qr.js
@@ -2,7 +2,7 @@ import QRCode from "qrcode";
 
 // ✅ For storing as value to embed in a QR (short URL)
 export const generateCanQrUrl = (canId) => {
-    return `https://localhost:5173/maintenance-form/${canId}`;
+    return `https://canrouteweb.netlify.app/maintenance-form/${canId}`;
 };
 
 // ✅ For generating base64 for printing (if needed)
